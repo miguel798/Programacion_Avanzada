d8b72dfa8eb734690eae98e5fc327980c831edcc
MIGUE <MIGUE@LAPTOP-QK3VSMAS>
2021-04-12 12:27:29 -0500

    Proyectos de NetBeans

--- src/programacion_avanzada/Main.java
+++ src/programacion_avanzada/Main.java
@@ -6,6 +6,8 @@
 package programacion_avanzada;
 
 import POO.Fecha;
+import POO.Alumno;
+import POO.Animal;
 
 /**
  *
@@ -17,7 +19,11 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Fecha hoy = new Fecha();
+        Fecha hoy = new Fecha(29, 7, 1987);
+        Fecha ayer = new Fecha();
+        ayer.anio = 1987;
+        //Alumno alumno1 = new Alumno("Juan", "Solis", 19, 2021670045, 5);
+        Animal animal1 = new Animal("Betobeen", "Gato", "Na", 5, 25);
         System.out.println("Hola Mundo");
     }
     
